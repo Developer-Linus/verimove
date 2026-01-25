39d416799dae46958731dea218065d11de6d7b26
Ezra-Kiprotich-Langat <ezralangat377@gmail.com>
2026-01-25 17:57:15 +0300

    feat: add attendance model

--- attendances/models.py
+++ attendances/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from staffs.models import StaffModel
+from vehicle_logs.models import CheckInModel
 
-# Create your models here.
+class SourceChoices(models.TextChoices):
+    AUTO = "AUTO", "Automatic(Vehicle Entry)"
+    MANUAL = "MANUAL", "Manual Entry"
+
+class AttendanceRecordModel(models.Model):
+    staff = models.ForeignKey(StaffModel, on_delete=models.CASCADE, related_name="attendace_records")
+    date = models.DateField(db_index=True)
+    first_checkin= models.ForeignKey(CheckInModel, on_delete=models.SET_NULL, null= True, blank=True, related_name = "attendace_first_checkin")
+    source = models.CharField(choices=SourceChoices, max_length=100, default=SourceChoices.AUTO)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        constraints= [
+            models.UniqueConstraint(fields=["staff", "date"], name="unique_staff_attendance"),
+            ]
+        ordering= ["-date"]
+    def __str__(self)->str:
+        return f"{self.staff.staff_number} - {self.date} ({self.source})"
