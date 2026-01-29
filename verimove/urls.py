e1a17275efb89dd15bf8e9339d08ca47a7176b7e
Developer-Linus <linusmksu30@gmail.com>
2026-01-29 15:55:51 +0300

    feat: add homepage and generate allowances form

--- verimove/urls.py
+++ verimove/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('about/', views.about),
     path('', include('staffs.urls')),
     path('', include('allowances.urls')),
     path('', include('vehicle_logs.urls')),
